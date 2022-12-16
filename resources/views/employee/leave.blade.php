831c72ed8a45a4c7152281d066ee6289b6503db3
gautam <gautamghand820@gmail.com>
2022-12-16 15:40:05 +0530

    frontend validation added and added old

--- resources/views/employee/leave.blade.php
+++ resources/views/employee/leave.blade.php
@@ -1,18 +1,16 @@
 @include('layouts.main')
 
-
-
 <section>
     <form method="POST" action="{{ route('employee.leave.store') }}">
         @csrf
         <label>Leave Subject</label>
-        <input type="text" name="subject">
+        <input type="text" name="subject" required value="{{ old('subject') }}">
         <x-error name="subject"/>
         <label>Leave Description</label>
-        <textarea name="description"></textarea>
+        <textarea name="description" required>{{ old('description') }}</textarea>
         <x-error name="description" />
         <label>Leave Date</label>
-        <input type="date" name="leave_dates">
+        <input type="date" name="leave_dates" required>
         <x-error name="leave_dates" />
         <input type="submit" name="submit" value="Take Leave" class="btn btn-primary">
         <a href="{{ route('employee') }}" class="btn btn-primary">Cancel</a>
